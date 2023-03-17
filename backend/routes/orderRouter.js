21b81fc2ff56745292df0f00869b04303d06d357
Madu Chiemelie Precious <101350287+Maduemelie@users.noreply.github.com>
2023-03-17 23:40:10 +0100

    backend

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -4,6 +4,8 @@ const controller = require('../controllers/orderController')
 
 router.route('/').post(controller.createOrder)
 router.route('/').get(controller.userOrderHistory)
+router.route('/:id').get(controller.getCurrentOrder)
+router.route('/:id').patch(controller.cancelOrder)
 
 
 module.exports = router
